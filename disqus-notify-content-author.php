4b0342b857076b393a0c81dfdad2c06df35dcdf8
jcederberg <jcederberg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-23 04:11:21 +0000

    Add object data dumping to v1.1.RC1
    git-svn-id: https://plugins.svn.wordpress.org/disqus-notify-content-author/trunk@1052198 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- disqus-notify-content-author.php
+++ disqus-notify-content-author.php
@@ -12,20 +12,6 @@
 defined('ABSPATH') or die("See no evil, hear no evil, speak no evil");
 
 /**
- * Action for notifying author of comments/pingbacks
- *
- * The main action hook for notifying authors when new comments
- * or pingbacks are added to posts and pages.
- *
- * @author jcederberg
- */
-function disqus_notify_content_author($comment_id, $comment_obj = null) {
-	if ( _disqus_notify_content_author__is_not_spam($comment_obj) ) {
-		wp_notify_postauthor($comment_id);
-	}
-}
-
-/**
  * Check if a pingback is marked as spam (by for example Akismet)
  *
  * With pingbacks being sent to the site while also using Disqus,
@@ -38,6 +24,31 @@ function disqus_notify_content_author($comment_id, $comment_obj = null) {
 function _disqus_notify_content_author__is_not_spam($comment_obj) {
 	$comment_status = $comment_obj->comment_approved;
 	return ( $comment_status != 'spam' );
+}
+
+/**
+ * Write comment data dump to WordPress root for debugging
+ */
+function _disqus_notify_content_author__comment_dump($id, $comment_obj) {
+	ob_start();
+	var_dump($comment_obj);
+	$out = ob_get_flush();
+	file_put_contents('comment_dump' .$id. '.txt', $out);
+}
+
+/**
+ * Action for notifying author of comments/pingbacks
+ *
+ * The main action hook for notifying authors when new comments
+ * or pingbacks are added to posts and pages.
+ *
+ * @author jcederberg
+ */
+function disqus_notify_content_author($comment_id, $comment_obj = null) {
+	_disqus_notify_content_author__comment_dump($comment_id, $comment_obj);
+	if ( _disqus_notify_content_author__is_not_spam($comment_obj) ) {
+		wp_notify_postauthor($comment_id);
+	}
 }
 
 add_action('wp_insert_comment', 'disqus_notify_content_author', 99, 2);
